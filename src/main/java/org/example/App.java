165ce00d66b0d7637815dd4d85831c68df95f59d
Joseph <JosephSchander@gmail.com>
2021-09-12 21:09:07 -0400

    2

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,7 +1,11 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 Joseph Schander
+ */
 package org.example;
 
 
-import java.text.DecimalFormat;
+
 import java.util.Scanner;
 
 public class App
@@ -13,7 +17,7 @@ public class App
         Scanner uInput = new Scanner(System.in);
         System.out.println( "Enter the password" );
         String Password = uInput.nextLine();
-        if(Password.equals("12345!A") == true){
+        if(Password.equals("12345!A")){
             System.out.println("Welcome!");
         }
         else{
